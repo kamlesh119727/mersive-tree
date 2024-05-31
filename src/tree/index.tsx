1a9c641ae42c97118bf62f07c6c8addb9f08a6be
kamlesh119727 <kamlesh119727@gmail.com>
2024-05-31 18:27:21 +0530

    added json and folder functions

--- src/tree/index.tsx
+++ src/tree/index.tsx
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
+import { TreeTypes } from "./TreeTypes";
 import "./index.css";
 
-const Tree = () => {
+const Tree = ({ name, file }: TreeTypes) => {
+  const [expand, setExpand] = useState(false);
+  if (typeof file !== "object") return <span>{file}</span>;
   return (
     <div className="tree">
-      root
+      {/* root
       <br />
       &nbsp;&nbsp;&nbsp;&nbsp;ant <br />
       &nbsp;&nbsp;&nbsp;&nbsp;bear <br />
@@ -12,7 +16,25 @@ const Tree = () => {
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant{" "}
       <br />
       &nbsp;&nbsp;&nbsp;&nbsp;frog <br />
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dolphin <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dolphin <br /> */}
+
+      <span
+        style={{ display: "flex", gap: 3 }}
+        onClick={() => setExpand(!expand)}
+      >
+        <div style={{ paddingLeft: "6px", paddingRight: "6px", width: "20px" }}>
+          {expand ? "-" : "+"}
+        </div>
+        {name}
+      </span>
+      <br />
+      <div style={{ display: expand ? "block" : "none", paddingLeft: 15 }}>
+        {Object.entries(file).map(([key, value]) => (
+          <>
+            <Tree key={key} name={key} file={value} />
+          </>
+        ))}
+      </div>
     </div>
   );
 };
